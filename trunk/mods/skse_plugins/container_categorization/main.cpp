02417962cddd153282b0ffe5040ff1424b0deb1a
rick <rick@384e5b21-ad0a-e111-a059-0030489379b8>
2011-11-30 23:43:53 +0000

    Update container categorization to support 1.2.12.0.

--- trunk/mods/skse_plugins/container_categorization/main.cpp
+++ trunk/mods/skse_plugins/container_categorization/main.cpp
@@ -31,7 +31,7 @@ bool plugin_query(const SKSEInterface *skse, PluginInfo *info)
 
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "container categorization";
-	info->version = 1;
+	info->version = 2;
 
 	if (skse->isEditor != 0)
 	{
@@ -39,7 +39,8 @@ bool plugin_query(const SKSEInterface *skse, PluginInfo *info)
 		return false;
 	}
 
-	if (skse->runtimeVersion != 0x01150000)
+	if (skse->runtimeVersion != 0x01150000 &&
+		skse->runtimeVersion != 0x020C0000)
 	{
 		_ERROR("unsupported runtime version");
 		return false;
@@ -52,27 +53,30 @@ bool plugin_query(const SKSEInterface *skse, PluginInfo *info)
 bool plugin_load(const SKSEInterface *skse)
 {
 	unsigned char nops[] = { 0x90, 0x90 };
-	unsigned char orig[] = { 0x75, 0x20 };
+	unsigned char original[] = { 0x75, 0x20 };
 
 	HMODULE exe = GetModuleHandle(NULL);
 
-	if (skse->runtimeVersion == 0x01150000)
-	{
-		unsigned int target = adjust_address(exe, 0x009A0035);
+	unsigned int target = 0;
 
-		if (memcmp((void *)target, orig, 2) != 0)
+	switch (skse->runtimeVersion)
+	{
+		case 0x01150000: target = 0x009A0035; break;
+		case 0x020C0000: target = 0x009A0AF5; break;
+		
+		default:
 		{
-			_ERROR("bad address?");
 			return false;
 		}
-		else
-		{
-			_MESSAGE("patch is a go");
-			return patch_code(target, nops, 2);
-		}
 	}
-	else
+
+	target = adjust_address(exe, target);
+	if (memcmp((void *)target, original, 2) != 0)
 	{
+		_ERROR("code at target is different, bad address?");
 		return false;
 	}
+
+	_MESSAGE("applying patching");
+	return patch_code(target, nops, 2);
 }
